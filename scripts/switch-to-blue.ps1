d60aa02fedc4392ad34a026c2620c888a2940b77
Nitin Singh <nitin.rathore@hotmail.com>
2026-09-06 23:28:19 +0530

    Delete scripts directory

--- scripts/switch-to-blue.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Rolling production traffic back to BLUE..."
-kubectl patch service frontend-service -p '{"spec":{"selector":{"app":"frontend","version":"blue"}}}'
-kubectl patch service frontend-service -p '{"spec":{"ports":[{"name":"http","port":80,"targetPort":3100}]}}'
-kubectl get service frontend-service
-kubectl get endpoints frontend-service
--- scripts/switch-to-green.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Switching production traffic to GREEN..."
-kubectl patch service frontend-service -p '{"spec":{"selector":{"app":"frontend","version":"green"}}}'
-kubectl patch service frontend-service -p '{"spec":{"ports":[{"name":"http","port":80,"targetPort":3200}]}}'
-kubectl get service frontend-service
-kubectl get endpoints frontend-service
